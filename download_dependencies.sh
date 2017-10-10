d52c010c43f1589ff5a7e68e96e6745bf93113cb
ramsey <ramseybissex@gmail.com>
2017-10-10 19:10:52 +0000

    adding a file to download dependencies this is a work in progress

---
+++ download_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+	echo "This probaby will not work without sudo"
+	exit
+fi
+
+apt-get install python-gps
+apt-get install gpsd gpsd-clients
+pip install RPi.GPIO
+apt-get install python-smbus
+apt-get install i2c-tools
+#Adafruit_ADS1x15 (https://github.com/adafruit/Adafruit_ADS1X15)
+#it-hook='import sys; sys.path.append("/path/to/root")'
